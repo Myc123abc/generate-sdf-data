92f9a79657bdf57a059d0bdf9014a6cab623f8dc
myc <1351579650@qq.com>
2025-08-30 20:30:15 +0800

    add ascender and height

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ std::string program_name;
 std::string output_file;
 std::string unicode_str;
 uint32_t    pixel_size;
+float       ascender;
+float       height;
 
 void print_help()
 {
@@ -26,8 +28,8 @@ void check(bool b, char const* msg)
 
 void store_sdf_bitmap(FT_GlyphSlot glyph_slot)
 {
-  auto advance_x = glyph_slot->advance.x >> 6;
-  auto advance_y = glyph_slot->advance.y >> 6;
+  auto advance_x = static_cast<float>(glyph_slot->advance.x) / 64;
+  auto advance_y = static_cast<float>(glyph_slot->advance.y) / 64;
 
   auto bitmap = glyph_slot->bitmap;
 
@@ -35,14 +37,16 @@ void store_sdf_bitmap(FT_GlyphSlot glyph_slot)
   file << "#ifndef GLYPH_SDF_BITMAP_H\n"
        << "#define GLYPH_SDF_BITMAP_H\n"
        << "\n"
-       << "#define Glyph_Unicode "     << unicode_str             << "\n"
-       << "#define Glyph_Width  "      << bitmap.width            << "\n"
-       << "#define Glyph_Height "      << bitmap.rows             << "\n"
-       << "#define Glyph_Left_Offset " << glyph_slot->bitmap_left << "\n"
-       << "#define Glyph_Up_Offset "   << -glyph_slot->bitmap_top << "\n"
-       << "#define Glyph_Size "        << pixel_size              << "\n"
-       << "#define Glyph_Advance_X "   << advance_x               << "\n"
-       << "#define Glyph_Advance_Y "   << advance_y               << "\n"
+       << "#define Glyph_Unicode "       << unicode_str             << "\n"
+       << "#define Glyph_Width  "        << bitmap.width            << "\n"
+       << "#define Glyph_Height "        << bitmap.rows             << "\n"
+       << "#define Glyph_Left_Offset "   << glyph_slot->bitmap_left << "\n"
+       << "#define Glyph_Up_Offset "     << -glyph_slot->bitmap_top << "\n"
+       << "#define Glyph_Size "          << pixel_size              << "\n"
+       << "#define Glyph_Advance_X "     << advance_x               << "\n"
+       << "#define Glyph_Advance_Y "     << advance_y               << "\n"
+       << "#define Glyph_Font_Ascender " << ascender                << "\n"
+       << "#define Glyph_Font_Height "   << height                  << "\n"
        << "static const unsigned char Glyph_SDF_Bitmap[] = {\n";
 
   for (auto i = 0; i < bitmap.rows; ++i)
@@ -87,6 +91,9 @@ int main(int argc, char** argv)
 
   FT_Load_Glyph(face, glyph_index, FT_LOAD_RENDER);
   FT_Render_Glyph(face->glyph, FT_RENDER_MODE_SDF);
+
+  ascender = static_cast<float>(face->ascender) * pixel_size / face->units_per_EM;
+  height   = static_cast<float>(face->height)   * pixel_size / face->units_per_EM;
 
   store_sdf_bitmap(face->glyph);
   
